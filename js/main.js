de9fbc1a68e9251755956bf55c3450527dbeba1f
mt-0509 <ppsr5504@yahoo.co.jp>
2020-03-17 15:32:33 +0900

    外部APIの情報を取得

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
 {
-  
+  // 外部APIを呼び出す
+  var request = new XMLHttpRequest();
+  request.open('GET', 'https://opentdb.com/api.php?amount=10', true);
+  request.responseType = 'json';
+
+  // レスポンスが帰って来たときの処理
+  request.onload = function () {
+    var data = this.response;
+    
+    // documentに表示する
+    document.getElementById('problem').innerHTML = data;
+  };
+
+  request.send();
 }

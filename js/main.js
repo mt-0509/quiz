75da486112550ac7e1d29223bcb4aadbfad27ae0
mt-0509 <ppsr5504@yahoo.co.jp>
2020-03-18 14:30:14 +0900

    fetch関数の書き方に変更 配列の使い方を調べる データを一つずつ使えるようになるかも

--- js/main.js
+++ js/main.js
@@ -2,17 +2,28 @@
 
 {
   // 外部APIを呼び出す
-  var request = new XMLHttpRequest();
-  request.open('GET', 'https://opentdb.com/api.php?amount=10', true);
-  request.responseType = 'json';
+  // var request = new XMLHttpRequest();
+  // request.open('GET', 'https://opentdb.com/api.php?amount=10', true);
+  // request.responseType = 'json';
 
-  // レスポンスが帰って来たときの処理
-  request.onload = function () {
-    var data = this.response;
+  // // レスポンスが帰って来たときの処理
+  // request.onload = function () {
+  //   var data = this.response;
     
-    // documentに表示する 3/17表示の仕方が不明
-    document.getElementById('problem').innerHTML = data;
-  };
+  //   // documentに表示する 3/17表示の仕方が不明
+  //   // document.getElementById('problem').innerHTML = data;
+  //   console.log(data);
+  // };
 
-  request.send();
+  // request.send();
+
+
+  const DATA_URL = 'https://opentdb.com/api.php?amount=10';
+  fetch(DATA_URL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonDATA) {
+    console.log(jsonDATA);
+  })
 }

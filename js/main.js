25c506813be1f2763817405af2419619ef3c9908
mt-0509 <ppsr5504@yahoo.co.jp>
2020-03-18 23:53:54 +0900

    jsonファイルをparseして配列にしないといけない事はわかった しかし、うまく機能しない 何が原因か不明

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
 'use strict';
 
 {
-  // 外部APIを呼び出す
-  // var request = new XMLHttpRequest();
-  // request.open('GET', 'https://opentdb.com/api.php?amount=10', true);
-  // request.responseType = 'json';
-
-  // // レスポンスが帰って来たときの処理
-  // request.onload = function () {
-  //   var data = this.response;
-    
-  //   // documentに表示する 3/17表示の仕方が不明
-  //   // document.getElementById('problem').innerHTML = data;
-  //   console.log(data);
-  // };
-
-  // request.send();
-
 
   const DATA_URL = 'https://opentdb.com/api.php?amount=10';
   fetch(DATA_URL)
@@ -24,6 +8,15 @@
     return response.json();
   })
   .then(function(jsonDATA) {
-    console.log(jsonDATA);
+    const json = {jsonDATA};
+    const obj = JSON.parse(json);
+    
+
+    console.log(obj.length);
+    
+    
+
   })
+
+  
 }

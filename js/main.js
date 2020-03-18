b94069d5c17538e6b60a4181e28a66f0fd89c66e
mt-0509 <ppsr5504@yahoo.co.jp>
2020-03-18 13:53:39 +0900

    とりあえずdocumentに表示することができた

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@
   // 外部APIを呼び出す
   var request = new XMLHttpRequest();
   request.open('GET', 'https://opentdb.com/api.php?amount=10', true);
-  request.responseType = 'json';
+  request.responseType = 'text';
 
   // レスポンスが帰って来たときの処理
   request.onload = function () {
     var data = this.response;
     
-    // documentに表示する 3/17表示の仕方が不明
+    // documentに表示する 
     document.getElementById('problem').innerHTML = data;
   };
 

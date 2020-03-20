60bc9896897c84aa5b3e1d6e15f345136eb52fd8
mt-0509 <ppsr5504@yahoo.co.jp>
2020-03-20 12:48:03 +0900

    問題文が次々に切り替わるようにするやり方不明

--- js/main.js
+++ js/main.js
@@ -1,25 +1,45 @@
 'use strict';
 
 {
-  const DATA_URL = 'https://opentdb.com/api.php?amount=10';
-  fetch(DATA_URL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(jsonDATA) {
-    var obj = jsonDATA;
-    // オブジェクトを取り出す
-    console.log(obj.results[1]);
+  // 開始ボタンを押したらスタートする
+  document.getElementById('start').addEventListener('click', () => {
 
-    // 取得したデータを表示する
-    // 問題文
-    document.getElementById('question').innerHTML = obj.results[1].question;
+    const DATA_URL = 'https://opentdb.com/api.php?amount=10';
+    fetch(DATA_URL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(jsonDATA) {
+      var obj = jsonDATA;
+      // オブジェクトを取り出す(不要な部分)
+      console.log(obj.results[1]);
 
-    // カテゴリー
-    document.getElementById('category').innerHTML = obj.results[1].category;
+      
 
-    // 難易度
-    document.getElementById('difficulty').innerHTML = obj.results[1].difficulty;
+      // 回答ボタンを追加(選択肢の数が異なる場合のやり方不明)
+      let btn = document.createElement('button');
+      document.getElementById('answer-one').appendChild(btn);
+      btn.innerHTML = obj.results[1].correct_answer;
+      
+      
+
+      // for (objre = 0; objre < 10; ++objre) {
+      //   document.getElementById('question').innerHTML = objre[i].question;
+
+      // }
+  
+      // 取得したデータを表示する
+      // 問題文
+      document.getElementById('question').innerHTML = obj.results[1].question;
+  
+      // カテゴリー
+      document.getElementById('category').innerHTML = obj.results[1].category;
+  
+      // 難易度
+      document.getElementById('difficulty').innerHTML = obj.results[1].difficulty;
+  });
+
+
 
 
   })

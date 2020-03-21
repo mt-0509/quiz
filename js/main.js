b7aff1044334337d0a292325ebb0de33f733eeb7
mt-0509 <ppsr5504@yahoo.co.jp>
2020-03-21 17:26:33 +0900

    選択肢をランダムに表示するところから

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@
 {
   // 開始ボタンを押したらスタートする
   document.getElementById('start').addEventListener('click', () => {
+    document.getElementById('question').innerHTML = '少々お待ちください';
           
       // 最初に非同期処理を行う
       function quiz() {
         return new Promise(function (resolve, reject) {
-          
-
+          // setTimeout(function () {
+          //   resolve(console.log('非同期成功'));
+          // }, 5000);
         });
       }
 
@@ -20,15 +22,50 @@
       return response.json();
     })
     .then(function(jsonDATA) {
+
       var obj = jsonDATA;
-      // 回答ボタンを追加(選択肢の数が異なる場合のやり方不明)
-      let btn = document.createElement('button');
-      document.getElementById('answer-one').appendChild(btn);
-      btn.innerHTML = obj.results[1].correct_answer;
+
+      console.log(jsonDATA);
+
+      // 回答ボタンを追加
+      let btnone = document.createElement('button');
+      let btntwo = document.createElement('button');
+      let btnthree = document.createElement('button');
+      let btnfour = document.createElement('button');
+
+      document.getElementById('answer-one').appendChild(btnone);
+      btnone.innerHTML = obj.results[0].correct_answer;
+
+      document.getElementById('answer-two').appendChild(btntwo);
+      btntwo.innerHTML = obj.results[0].incorrect_answers[0];
+
+      document.getElementById('answer-three').appendChild(btnthree);
+      btnthree.innerHTML = obj.results[0].incorrect_answers[1];
+
+      document.getElementById('answer-four').appendChild(btnfour);
+      btnfour.innerHTML = obj.results[0].incorrect_answers[2];
+
+      // ランダムに使う配列
+      var arr = [btnone, btntwo, btnthree, btnfour];
+      var a = arr.length;
+
+      // シャッフルアルゴリズム
+      while (a) {
+        var j = Math.floor( Math.random() * a );
+        var t = arr[--a];
+        arr[a] = arr[j];
+        arr[j] = t;
+      }
+
+      // シャッフルされた配列の要素を順番に表示する
+      arr.forEach( function (value) {
+        document.getElementById('answer-one').value;
+        document.getElementById('answer-two').value;
+        document.getElementById('answer-three').value;
+        document.getElementById('answer-four').value;
+        
+      });
       
-      // for (objre = 0; objre < 10; ++objre) {
-      //   document.getElementById('question').innerHTML = objre[i].question;
-      // }
   
       // 取得したデータを表示する
       // 問題文
